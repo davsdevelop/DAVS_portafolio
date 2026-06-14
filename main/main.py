0fbc64f1a58d1205fcc4ad312a26c1086230aadd
DAVS <dvidelasilva@gmail.com>
2026-06-14 18:00:09 -0400

    se agrega nombre a pestaña

--- main/main.py
+++ main/main.py
@@ -16,7 +16,11 @@ from .pages.projects_pages.reflex_page import proyectos_reflex_page
 from .pages.projects_pages.sql_page import proyectos_sql_page
 
 
-
+@rx.page(
+    route="/", 
+    title="DAVSportafolio",  
+    description="Mi portafolio profesional",
+)
 def index() -> rx.Component:
     return rx.vstack(
         
@@ -41,12 +45,12 @@ app = rx.App(
 )
 app.add_page(index)
 
-app.add_page(experiencia_page(), route="/experiencia")
-app.add_page(estudios_page(), route="/estudios")
-app.add_page(sobre_mi_page(), route="/contacto")
-app.add_page(proyectos_python_page(), route="/proyectospython")
-app.add_page(proyectos_fastapi_page(), route="/proyectosfastapi")
-app.add_page(proyectos_django_page(), route="/proyectosdjango")
-app.add_page(proyectos_reflex_page(), route="/proyectosreflex")
-app.add_page(proyectos_sql_page(), route="/proyectossql")
-app.add_page(ml_page(), route="/proyectosML")
+app.add_page(experiencia_page(), route="/experiencia", title="DAVSportafolio | Experiencia")
+app.add_page(estudios_page(), route="/estudios", title="DAVSportafolio | Estudios")
+app.add_page(sobre_mi_page(), route="/contacto", title="DAVSportafolio | Contacto")
+app.add_page(proyectos_python_page(), route="/proyectospython", title="DAVSportafolio | Python")
+app.add_page(proyectos_fastapi_page(), route="/proyectosfastapi", title="DAVSportafolio | FastAPI")
+app.add_page(proyectos_django_page(), route="/proyectosdjango", title="DAVSportafolio | Django")
+app.add_page(proyectos_reflex_page(), route="/proyectosreflex", title="DAVSportafolio | Reflex")
+app.add_page(proyectos_sql_page(), route="/proyectossql", title="DAVSportafolio | SQL")
+app.add_page(ml_page(), route="/proyectosML", title="DAVSportafolio | Machine Learning")
